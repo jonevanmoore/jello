f22ce500c77d8d0343fa441ba50398966b3d8a62
Jon Moore <jonevanmoore@yahoo.com>
2022-05-02 18:15:11 -0500

    folders organized and landing page started
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import LogoutButton from './LogoutButton';
 
 import './NavBar.css'
 
