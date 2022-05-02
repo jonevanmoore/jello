11bce6f6d85412fb6e875331cc16b94e2ac2c009
Jon Moore <jonevanmoore@yahoo.com>
2022-05-02 16:19:18 -0500

    user model updated
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,15 +1,21 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    avatar_id = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.Datetime(timezone=True),
+                           server_default=func.now())
+    updated_at = db.Column(db.Datetime(timezone=True), onupdate=func.now())
 
     @property
     def password(self):
