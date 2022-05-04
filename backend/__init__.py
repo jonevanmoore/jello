e6264be57abbdb37874e8dbda1f720e77ed46a26
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-04 18:18:38 -0500

    register list blueprint

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,9 +6,10 @@ from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
 from .models import db, User
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
+from .api.user_routes  import user_routes
+from .api.auth_routes  import auth_routes
 from .api.board_routes import board_routes
+from .api.list_routes  import list_routes
 
 from .seeds import seed_commands
 
@@ -30,9 +31,10 @@ def load_user(id):
 app.cli.add_command(seed_commands)
 
 app.config.from_object(Config)
-app.register_blueprint(user_routes, url_prefix='/api/users')
-app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(user_routes,  url_prefix='/api/users')
+app.register_blueprint(auth_routes,  url_prefix='/api/auth')
 app.register_blueprint(board_routes, url_prefix='/api/boards')
+app.register_blueprint(list_routes,  url_prefix='/api/lists')
 db.init_app(app)
 Migrate(app, db)
 
