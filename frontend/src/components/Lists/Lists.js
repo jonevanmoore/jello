b486722238b1412a84fe5e948f0687778109dcf3
MasonKogami <maskogami@gmail.com>
2022-05-05 14:48:09 -0700

    add react-beautiful-dnd package, add dragdropcontext, droppable, draggable
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

--- frontend/src/components/Lists/Lists.js
+++ frontend/src/components/Lists/Lists.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Redirect, useHistory, useParams } from 'react-router-dom';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import { readOneBoard, createList } from '../../store/boards';
 
@@ -24,47 +25,57 @@ const ListsPage = () => {
         await dispatch(createList(newList))
     }
 
-    // console.log('LISTS: ', board.lists);
+    console.log('LISTS: ', board.lists);
 
     return (
         <div className='lists__in__boards'>
-            <div className='list__size'>
-                {board.lists.map(list =>
-                    <div className='list__container'>
-                        <div className='list__title__close'>
-                            <label className='list__title'>
-                                {list.title}
-                            </label>
-                            <button class="close">
-                                <div class="close__text">&#215;</div>
-                            </button>
-                        </div>
-                        <div>
-                            {list.cards.map(card =>
-                                <div className='card__container'>
-                                    <div className='card__content'>{card.content}</div>
-                                    <div className='card__description'>{card.description}</div>
-                                    <div className='card__due__date'>{card.due_date}</div>
-                                    {/* <div>{card.created_at}</div> */}
-                                </div>
+            <DragDropContext>
+                <Droppable droppableId='lists'>
+                    {(provided) => {
+                        <div className='list__size' {...provided.droppableProps} ref={provided.innerRef}>
+                            {board.lists.map((list, index) =>
+                            <Draggable draggableId={index} key={index} index={index}>
+                                {(provided) => {
+                                    <div className='list__container' {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
+                                        <div className='list__title__close'>
+                                            <label className='list__title'>
+                                                {list.title}
+                                            </label>
+                                            <button class="close">
+                                                <div class="close__text">&#215;</div>
+                                            </button>
+                                        </div>
+                                        <div>
+                                            {list.cards.map(card =>
+                                                <div className='card__container'>
+                                                    <div className='card__content'>{card.content}</div>
+                                                    <div className='card__description'>{card.description}</div>
+                                                    <div className='card__due__date'>{card.due_date}</div>
+                                                    {/* <div>{card.created_at}</div> */}
+                                                </div>
+                                            )}
+                                        </div>
+
+                                        <div className='create__list__button'>
+                                            <button
+                                                id='lists__buttons'
+                                                className='
+                                        light__green__blue__button
+                                        jello__wiggle
+                                        button__shine__short
+                                        '>
+                                                Create a Card
+                                            </button>
+
+                                        </div>
+                                    </div>
+                                }}
+                            </Draggable>
                             )}
                         </div>
-
-                        <div className='create__list__button'>
-                            <button
-                                id='lists__buttons'
-                                className='
-                        light__green__blue__button
-                        jello__wiggle
-                        button__shine__short
-                        '>
-                                Create a Card
-                            </button>
-
-                        </div>
-                    </div>
-                )}
-            </div>
+                    }}
+                </Droppable>
+            </DragDropContext>
             {/* <div className='lists__in__boards'> */}
             <div className='list__size'>
 
