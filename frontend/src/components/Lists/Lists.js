42f3d7bf4129635749f1582035c97615c2e01eb5
Jon Moore <jonevanmoore@yahoo.com>
2022-05-05 15:35:20 -0500

    can now create a list
    Co-authored-by: Jon Moore <jonevanmoore@yahoo.com>
    Co-authored-by: Mason Kogami <maskogami@gmail.com>
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>
    Co-authored-by: Daniel Blanco <db@d-blanco.com>

--- frontend/src/components/Lists/Lists.js
+++ frontend/src/components/Lists/Lists.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, Redirect, useHistory, useParams } from 'react-router-dom';
 
-import { readOneBoard } from '../../store/boards';
+import { readOneBoard, createList } from '../../store/boards';
 
 import './Lists.css';
 import './Cards.css';
@@ -11,6 +11,18 @@ const ListsPage = () => {
 
     const { board_id } = useParams();
     const board = useSelector(state => state.boards[board_id]);
+    const sessionUser = useSelector(state => state.session.user);
+    const user_id = sessionUser.id;
+
+    const dispatch = useDispatch()
+
+    const [addListBtnDisplay, setAddListBtnDisplay] = useState('displayed')
+    const [title, setTitle] = useState('')
+
+    const addNewList = async () => {
+        const newList = { title, user_id, board_id, order: board.lists.length }
+        await dispatch(createList(newList))
+    }
 
     // console.log('LISTS: ', board.lists);
 
@@ -46,7 +58,7 @@ const ListsPage = () => {
                         jello__wiggle
                         button__shine__short
                         '>
-                                Create a List
+                                Create a Card
                             </button>
 
                         </div>
@@ -57,6 +69,22 @@ const ListsPage = () => {
             <div className='list__size'>
 
                 <div className='list__container'>
+                    <div className='add-another-list-div'>
+                        <input
+                            onChange={e => setTitle(e.target.value)}
+                            type='text'
+                            value={title}
+                        >
+                        </input>
+                        <div className='create-list-btns'>
+                            <button
+                                onClick={addNewList}
+                            >Create List</button>
+                            <button class="close">
+                                <div class="close__text">&#215;</div>
+                            </button>
+                        </div>
+                    </div>
                     <div className='create__list__button'>
                         <button
                             id='lists__buttons'
@@ -65,7 +93,7 @@ const ListsPage = () => {
                         jello__wiggle
                         button__shine__short
                         '>
-                            Add another List
+                            {board.lists.length > 0 ? 'Add Another List' : 'Create a List'}
                         </button>
 
                     </div>
