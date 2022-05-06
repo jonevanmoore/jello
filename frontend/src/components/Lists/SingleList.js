d1787a721597799ef1529f573ef054a20799e92b
Jon Moore <jonevanmoore@yahoo.com>
2022-05-06 18:50:24 -0500

    title style done

--- frontend/src/components/Lists/SingleList.js
+++ frontend/src/components/Lists/SingleList.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateList, deleteList } from '../../store/boards';
 import { useParams } from 'react-router-dom';
+import './SingleList.css'
 
 export const SingleList = ({ list }) => {
     const dispatch = useDispatch()
@@ -10,7 +11,8 @@ export const SingleList = ({ list }) => {
     const [titleDisplay, setTitleDisplay] = useState('displayed')
     const [titleInputDisplay, setTitleInputDisplay] = useState('not-displayed')
 
-
+    const [titleInputValid, setTitleInputValid] = useState('valid-blue')
+    const [titleSubmitBtn, setTitleSubmitBtn] = useState('able')
 
     const updateTitle = async () => {
         let updated = {
@@ -41,37 +43,60 @@ export const SingleList = ({ list }) => {
             setTitleInputDisplay('not-displayed')
             setTitleDisplay('displayed')
         }
+        setNewTitle(list.title)
     }
 
     const removeList = async (list) => {
         await dispatch(deleteList(list));
-        // TODO: fix this
     };
+
+    useEffect(() => {
+        if (newTitle.length < 30 && newTitle.length > 0) {
+            setTitleInputValid('valid-blue')
+            setTitleSubmitBtn('able')
+        } else {
+            setTitleInputValid('invalid')
+            setTitleSubmitBtn('disabled-blue')
+        }
+
+    }, [newTitle])
 
     return (
 
         <div className='list__title__close'>
 
             <div className='title-and-input-display'>
-                <label className={`list__title ${titleDisplay}`}>
-                    {list.title}
-                </label>
-                <div className={`edit-title-div ${titleInputDisplay}`}>
+                <div className='list-title-div'>
+                    <label className={`list__title ${titleDisplay}`}>
+                        {list.title}
+                    </label>
+                    <i className={`fa-solid fa-pen-to-square ${titleDisplay}`} onClick={titleAndInputDisplay}></i>
+                </div>
+                <div className={`edit-title-div ${titleInputDisplay} grow-down`}>
                     <input
                         type="text"
                         value={newTitle}
                         onChange={(e) => setNewTitle(e.target.value)}
+                        className="title-input"
                     ></input>
-                    <button onClick={updateTitle}>Update</button>
-                    <button onClick={titleAndInputDisplay}>Cancel</button>
+                    <div className={`check-div`}>
+                        <i className="fa-solid fa-circle-check" id={titleInputValid}></i>
+                    </div>
+                    <div className='update-title-btns'>
+                        <button
+                            onClick={updateTitle}
+                            className={`jello-wiggle button__shine__short light__blue__button ${titleSubmitBtn}`}
+                            disabled={titleSubmitBtn === 'disabled-blue'}
+                        >Update</button>
+                        <button onClick={titleAndInputDisplay} className='jello-wiggle cancel-title-btn'>Cancel</button>
+                    </div>
                 </div>
             </div>
-            <i className={`fa-solid fa-pen-to-square ${titleDisplay}`} onClick={titleAndInputDisplay}></i>
             <button
-                className="close"
+                className={`${titleDisplay} delete-list`}
                 onClick={() => removeList(list)}
             >
-                <div className={`close__text ${titleDisplay}`}>&#215;</div>
+                <div className={`close__text`}>&#215;</div>
             </button>
         </div>
 
