09917dfd1043eb676e2225676544c7889f793fda
MasonKogami <maskogami@gmail.com>
2022-05-06 10:47:37 -0700

    add new comment form, add create comment route
    Co-authored-by: Nick Yuan <NicholasNYuan@gmail.com>

--- backend/api/card_routes.py
+++ backend/api/card_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from flask_login import current_user, login_required
-from backend.models import db, Card
+from backend.models import db, Card, Comment
+from backend.forms import NewCommentForm
 
 
 card_routes = Blueprint("card_routes", __name__)
@@ -36,3 +37,22 @@ def delete_card(id):
     db.session.commit()
 
     return card.to_dict()
+
+
+# C R E A T E  C O M M E N T
+@card_routes.route('/<int:id>/comments', methods=['POST'])
+@login_required
+def new_comment(id):
+    comment = NewCommentForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        comment = Comment(
+            user_id=form.data['user_id'],
+            card_id=id,
+            body=form.data['content']
+        )
+        
+        db.session.add(comment)
+        db.session.commit()
+        return comment.to_dict()
