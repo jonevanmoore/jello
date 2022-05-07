7244017da4f524dc827ce41b8e72028425412376
Jon Moore <jonevanmoore@yahoo.com>
2022-05-07 17:33:33 -0500

    close modal

--- backend/api/board_routes.py
+++ backend/api/board_routes.py
@@ -82,10 +82,11 @@ def share_board(id):
     board = Board.query.get(id)
     email = request.json['email']
     user = User.query.filter(User.email == email).first()
-    print(user)
+
     if user:
         board.shared_users += [user]
     db.session.commit()
+
     return user.to_dict()
 
 # U P D A T E   L I S T   O R D E R
